6097ed6fea41837312780be4967b66a1bb0856fd
zhouxiaohui0327 <475503798@qq.com>
2015-09-29 08:40:59 +0800

    topic

---
+++ header.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 2015/9/28
+ * Time: 13:29
+ */
+
+?>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/header.css"/>
+
+</head>
+<body>
+<div class="header">
+    <div class="navbar container-fluid">
+        <div class="brand-navbar left">
+            <a class="brand-navbar" href=""><img src="http://public.tratao.com/aiysea/logo/aiysea.png" class="hide-t-md" alt=""/></a>
+            <a class="brand-navbar" href=""><img src="http://public.tratao.com/aiysea/logo/small/aiysea.png" class="hide-f-md" alt=""/></a>
+        </div>
+        <ul class="login right">
+            <li><a href="">登录</a></li>
+            <li><a href="">注册</a></li>
+        </ul>
+        <p class="number right">旅行顾问：<span> 400-670-9078</span></p>
+    </div>
+</div>
+</body>
+</html>
---
+++ topic.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 2015/9/28
+ * Time: 17:07
+ */
+include "header.php";
+?>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/header.css"/>
+    <link rel="stylesheet" type="text/css" href="css/topic.css"/>
+</head>
+<body>
+<div class="landingContainer topic-landingContainer">
+    <div class="landingContent">
+        <div class="thumbnail thumborder">
+            <div class="thumburl">
+                <picture>
+
+                    <source type="image/webp" data-sizes="auto" data-source="http://public.tratao.com/27a075c2a7b64690717e33f8a64e2b2d.webp 189w, http://public.tratao.com/6b5209a5cd5b4d7f7e5de25bb530b163.webp 2276w, http://public.tratao.com/9ea4f8563cbb4f2e5f4dce306679d24e.webp 3416w, http://public.tratao.com/8f54bc4fd27a451c79bb543ed8f247db.webp 1708w, http://public.tratao.com/be98ab74ec6a46a25d6bb3e22da29a96.webp 1518w, http://public.tratao.com/57bda4a7cb964ea57d099f7c620e0b10.webp 1138w, http://public.tratao.com/81a9ea3d145c4d76424026eb02b10db7.webp 378w, http://public.tratao.com/86a74ef998d644644df999ba009da66f.webp 759w, " sizes="1465px" srcset="http://public.tratao.com/27a075c2a7b64690717e33f8a64e2b2d.webp 189w, http://public.tratao.com/6b5209a5cd5b4d7f7e5de25bb530b163.webp 2276w, http://public.tratao.com/9ea4f8563cbb4f2e5f4dce306679d24e.webp 3416w, http://public.tratao.com/8f54bc4fd27a451c79bb543ed8f247db.webp 1708w, http://public.tratao.com/be98ab74ec6a46a25d6bb3e22da29a96.webp 1518w, http://public.tratao.com/57bda4a7cb964ea57d099f7c620e0b10.webp 1138w, http://public.tratao.com/81a9ea3d145c4d76424026eb02b10db7.webp 378w, http://public.tratao.com/86a74ef998d644644df999ba009da66f.webp 759w, ">
+                    <source type="image/jpeg" data-sizes="auto" data-source="http://public.tratao.com/50ab01f57527452d7925f14217e6086a.jpeg 1708w, http://public.tratao.com/3cd25b024000498948a7727211236292.jpeg 1138w, http://public.tratao.com/45550bd4c89743dd76725c775d63b6c7.jpeg 189w, http://public.tratao.com/048562b812624a4f660aabe5285bab85.jpeg 2276w, http://public.tratao.com/f4dda0e10aae4141565aa024fc1113ab.jpeg 759w, http://public.tratao.com/95f5b84297354e834324455ba5522695.jpeg 378w, http://public.tratao.com/5eea4d5f157947b0663590b6b1fd222a.jpeg 3416w, http://public.tratao.com/79d5e814288b45a64102adaeeb3d2903.jpeg 1518w, " sizes="1465px" srcset="http://public.tratao.com/50ab01f57527452d7925f14217e6086a.jpeg 1708w, http://public.tratao.com/3cd25b024000498948a7727211236292.jpeg 1138w, http://public.tratao.com/45550bd4c89743dd76725c775d63b6c7.jpeg 189w, http://public.tratao.com/048562b812624a4f660aabe5285bab85.jpeg 2276w, http://public.tratao.com/f4dda0e10aae4141565aa024fc1113ab.jpeg 759w, http://public.tratao.com/95f5b84297354e834324455ba5522695.jpeg 378w, http://public.tratao.com/5eea4d5f157947b0663590b6b1fd222a.jpeg 3416w, http://public.tratao.com/79d5e814288b45a64102adaeeb3d2903.jpeg 1518w, ">
+
+                    <img class="img-responsive  lazyautosizes lazyloaded" data-sizes="auto" data-source="http://public.tratao.com/50ab01f57527452d7925f14217e6086a.jpeg 1708w, http://public.tratao.com/3cd25b024000498948a7727211236292.jpeg 1138w, http://public.tratao.com/45550bd4c89743dd76725c775d63b6c7.jpeg 189w, http://public.tratao.com/048562b812624a4f660aabe5285bab85.jpeg 2276w, http://public.tratao.com/f4dda0e10aae4141565aa024fc1113ab.jpeg 759w, http://public.tratao.com/95f5b84297354e834324455ba5522695.jpeg 378w, http://public.tratao.com/5eea4d5f157947b0663590b6b1fd222a.jpeg 3416w, http://public.tratao.com/79d5e814288b45a64102adaeeb3d2903.jpeg 1518w, " sizes="1465px" srcset="http://public.tratao.com/50ab01f57527452d7925f14217e6086a.jpeg 1708w, http://public.tratao.com/3cd25b024000498948a7727211236292.jpeg 1138w, http://public.tratao.com/45550bd4c89743dd76725c775d63b6c7.jpeg 189w, http://public.tratao.com/048562b812624a4f660aabe5285bab85.jpeg 2276w, http://public.tratao.com/f4dda0e10aae4141565aa024fc1113ab.jpeg 759w, http://public.tratao.com/95f5b84297354e834324455ba5522695.jpeg 378w, http://public.tratao.com/5eea4d5f157947b0663590b6b1fd222a.jpeg 3416w, http://public.tratao.com/79d5e814288b45a64102adaeeb3d2903.jpeg 1518w, ">
+                </picture>
+                <div class="mask"></div>
+                <table>
+                    <tbody><tr>
+                        <td>
+                            <p class="title max-width470">一起走到，比永远还远</p>
+                        </td>
+                    </tr>
+                    </tbody></table>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
